35ef38bb645147d2334455f628df8910675783be
Macnacka <93340013+BaenaArnau@users.noreply.github.com>
2024-09-20 13:29:51 +0200

    Actualizacion del main

--- Formas2D/Program.cs
+++ Formas2D/Program.cs
@@ -10,43 +10,7 @@ namespace Formas2D
     {
         static void Main(string[] args)
         {
-            Elipse elipse = new Elipse(12,16);
-            Circulo circulo = new Circulo(8);
-            Rectangulo rectangulo = new Rectangulo(20,6);
-            Cuadrado cuadrado = new Cuadrado(6);
-            Rombo rombo = new Rombo(4,8);
-            Triangulo triangulo = new Triangulo(6,6,30);
             Diagrama diagrama = new Diagrama();
-
-            Console.WriteLine($@"
-{circulo.ToString()}
-Area del circulo: {circulo.Area()}
-Perimetro del circulo: {circulo.Perimetro()}");
-
-            Console.WriteLine($@"
-{rectangulo.ToString()}
-Area del rectangulo: {rectangulo.Area()}
-Perimetro del rectangulo: {rectangulo.Perimetro()}");
-
-            Console.WriteLine($@"
-{cuadrado.ToString()}
-Area del cuadrado: {cuadrado.Area()}
-Perimetro del cuadrado: {cuadrado.Perimetro()}");
-
-            Console.WriteLine($@"
-{rombo.ToString()}
-Area del rombo: {rombo.Area()}
-Perimetro del rombo: {rombo.Perimetro()}");
-
-            Console.WriteLine($@"
-{triangulo.ToString()}
-Area del triangulo: {triangulo.Area()}
-Perimetro del triangulo: {triangulo.Perimetro()}");
-
-            Console.WriteLine($@"
-{elipse.ToString()}
-Area del elipse: {elipse.Area()}
-Perimetro del elipse: {elipse.Perimetro()}");
 
             Console.WriteLine($@"
 Numero de figuras {diagrama.Figuras.Count}
